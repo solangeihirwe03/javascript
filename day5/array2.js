05820483565da827d074cbf75a04d4a1eb035edb
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-03-02 14:42:55 +0200

    day5 learning js

---
+++ day5/array2.js
@@ -0,0 +1,55 @@
+//let's create an array with values of "red", "green", "blue"
+
+let array = ["red", "green", "blue"];
+//then we're going to push orange at the end of our array
+
+array.push("yellow");
+console.log(array);
+//here to remove last value in array
+
+array.pop();
+console.log(array);
+//accessing second number in our array
+
+console.log(array[1]);
+
+//to check if our array have specified value example
+let check = array.includes("orange");
+
+console.log(check); //this will return false because orange we have already removed it
+
+//array iteration and manipulation
+//to display each color in our array
+
+for(let i = 0; i <= array.length; i++){
+    console.log(array[i]);
+}
+// double values of our array then return new array
+
+let numbers = [1, 4, 5, 2 , 9];
+
+numbers = numbers.map((num)=>num*2)
+
+console.log(numbers)
+
+// filter out even number in our array
+
+numbers = numbers.filter((num)=> num % 2!== 0);
+
+console.log(numbers);
+//displaying values within array
+
+array.forEach((arrDisplay)=>console.log(arrDisplay));
+
+//array searching and sorting
+
+//the index of the element "blue" in the colors array
+
+let indexOfBlue = array.indexOf("blue");
+
+console.log(indexOfBlue);
+
+//sorting numbers in ascending order
+
+let sortNumbers = numbers.sort((a, b)=> a - b );
+console.log(sortNumbers);
---
+++ object.js
@@ -0,0 +1,8 @@
+let num = -10.62345678;
+console.log(Math.abs(num));
+
+let number = Math.abs(num);
+console.log(Math.ceil(number));
+let random = Math.random(number);
+console.log(Math.round(number));
+console.log(random);
